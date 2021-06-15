5442893190ff8cfc3a3ac61a57247f02d10a2a12
Aaron Edsinger <aedsinger@hello-robot.com>
2021-06-14 18:57:55 -0700

    Added TimingStats and unittest

---
+++ body/test/test_timing_stats.py
@@ -0,0 +1,30 @@
+# Logging level must be set before importing any stretch_body class
+import stretch_body.robot_params
+# TODO: Fails under PY3!
+#stretch_body.robot_params.RobotParams.set_logging_level("DEBUG")
+
+import unittest
+import time
+import stretch_body.hello_utils as hello_utils
+import random
+import stretch_body.device
+d=stretch_body.device.Device() #Create logger
+
+class TestTimingStats(unittest.TestCase):
+    def test_faux_loop(self):
+        print('Starting test_faux_loop ')
+        target_loop_rate = 25.0
+        s = hello_utils.TimingStats(loop_name='TestLoop', target_loop_rate=target_loop_rate)
+        for i in range(200):
+            s.mark_loop_start()
+            time.sleep(0.5/ target_loop_rate) #Executing some computation
+            s.mark_loop_end()
+            noise_pct = random.random()*0.1
+            time.sleep(s.get_loop_sleep_time()*(1+noise_pct)) #Add jitter due to 'threading', etc
+        s.pretty_print()
+        s.display_rate_histogram()
+        self.assertTrue(s.status['loop_warns'] == 0)
+
+
+
+
